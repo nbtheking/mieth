2d988dc98d8a17178afc270438e3d2342ec2878f
nbtheking <nhialbiellok@gmail.com>
2022-05-28 18:25:00 +0200

    ""

--- App.js
+++ App.js
@@ -62,7 +62,6 @@ export default function App() {
           )}
           ListFooterComponent={renderFooter}
           onEndReached={handleLoadMore}
-          onEndReachedThreshold={0}
         />
       </View>
       <StatusBar />
